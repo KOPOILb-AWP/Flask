4355e4b4e172f4f9d52065a685ff8db3ed2d9a51
Pavel <sasasaloskina@gmail.com>
2020-09-22 16:40:44 +0400

    4 commit

--- 1.py
+++ 1.py
@@ -8,5 +8,22 @@ def hello_world():
 @app.route('/')
 def index():
     return 'Index Page'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
 if __name__ == '__main__':
     app.run()
