cc690347b76f31139eab0216ec36ed7bcdc2bb15
Pavel <sasasaloskina@gmail.com>
2020-09-22 17:04:13 +0400

    Проба визуализации шаблонов

--- 1.py
+++ 1.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello_world():
-    return 'Hello World!'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 @app.route('/')
 def index():
